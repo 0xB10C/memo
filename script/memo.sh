b8542248abb8d874118766d0fc35bd5a3194fb19
smadd <0xb10c@gmail.com>
2017-11-26 13:34:19 +0100

    cronjobs: added example cronjobs + cleaned up memo.sh

--- script/memo.sh
+++ script/memo.sh
@@ -5,14 +5,10 @@ BASEDIR="/path/to/memo"
 
 cd $BASEDIR
 
-# read mempool data and write to db
-python ./script/mempool_to_db.py
-
-if [ $1 -eq 3 ] # mempool stat identifyer
+if [ $1 -eq 3 ] # mempool stats only
 then
   python ./script/mempool_stats_to_db.py
+else
+  python ./script/mempool_to_db.py # read mempool data and write to db
+  python ./script/database_to_dygraph_csv.py $1 # generate csv files
 fi
-
-# generate csv files
-python ./script/bucketlevel_to_dygraph_csv.py
-python ./script/database_to_dygraph_csv.py $1
