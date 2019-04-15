b3e1f5e7e9898f955497342e83bcb77e5abf68bf
0xb10c <0xb10c@gmail.com>
2019-04-15 12:18:06 +0200

    remove unused range function

--- www/js/chart-api-test.js
+++ www/js/chart-api-test.js
@@ -259,15 +259,3 @@ function showAlert() {
 function clearAlerts() {
   $('.alert-tx').hide()
 }
-
-function* range(start, stop, step = 1) {
-  if (typeof stop === 'undefined') {
-      // one param defined
-      stop = start;
-      start = 0;
-  }
-
-  for (let i = start; step > 0 ? i < stop : i > stop; i += step) {
-      yield i;
-  }
-}
