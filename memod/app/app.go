ea682d65c74a37ecf016266f6c0875007a3ffc01
0xb10c <0xb10c@gmail.com>
2019-05-15 13:42:48 +0200

    Add exit signal handling

--- memod/app/app.go
+++ memod/app/app.go
@@ -1,6 +1,10 @@
 package app
 
 import (
+	"os"
+	"os/signal"
+	"syscall"
+
 	"github.com/0xb10c/memo/memod/database"
 	"github.com/0xb10c/memo/memod/logger"
 	"github.com/0xb10c/memo/memod/mempool"
@@ -18,6 +22,26 @@ func Start() {
 	}
 	defer db.Close()
 
-	mempool.SetupMempoolFetcher()
+	// run the mempool fetcher in a goroutine
+	go mempool.SetupMempoolFetcher()
 
+	waitForOSSignal()
+}
+
+func waitForOSSignal() {
+	exitSignals := make(chan os.Signal, 1)
+	shouldExit := make(chan bool, 1)
+
+	signal.Notify(exitSignals, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	go handleExitSig(exitSignals, shouldExit)
+
+	<-shouldExit // wait till memod should exit
+	logger.Info.Println("Memod exiting")
+}
+
+// handles exit signals
+func handleExitSig(exitSignals chan os.Signal, shouldExit chan bool) {
+	sig := <-exitSignals
+	logger.Info.Println("Received signal", sig)
+	shouldExit <- true
 }
