ed3b0a0d8684bb55ba3b07097929983f6d775e5d
0xb10c <0xb10c@gmail.com>
2019-05-15 10:25:33 +0200

    Add proper error handling

--- memod/mempool/writer.go
+++ memod/mempool/writer.go
@@ -1,6 +1,7 @@
 package mempool
 
 import (
+	"errors"
 	"time"
 
 	"github.com/0xb10c/memo/memod/database"
@@ -9,17 +10,18 @@ import (
 
 /* wirtes data to database */
 
-func writeCurrentMempoolData(feerateMapJSON string, mempoolSizeInByte int, megabyteMarkersJSON string) {
+func writeCurrentMempoolData(feerateMapJSON string, mempoolSizeInByte int, megabyteMarkersJSON string) error {
 	defer logger.TrackTime(time.Now(), "writeCurrentMempoolData()")
 
 	if database.Database != nil {
 		sql := "UPDATE current_mempool SET byCount = ?, positionsInGreedyBlocks = ?, timestamp = UTC_TIMESTAMP, mempoolSize = ? WHERE id = 1"
 		_, err := database.Database.Exec(sql, feerateMapJSON, megabyteMarkersJSON, mempoolSizeInByte)
 		if err != nil {
-			panic(err)
+			return err
 		}
-	} else {
-		logger.Error.Println("Database is nil")
+		return nil
 	}
+
+	return errors.New("Database pointer is nil")
 
 }
