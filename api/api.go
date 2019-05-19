1fb1ee21f855ad3f8a96d2705abfbc11f2e1bb3c
0xb10c <0xb10c@gmail.com>
2019-05-19 14:17:35 +0200

    Rewrote api in Go

---
+++ api/api.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"encoding/json"
+
+	"github.com/gin-gonic/gin"
+	"github.com/0xb10c/memo/api/database"
+	"github.com/0xb10c/memo/api/config"
+	"github.com/gin-contrib/cors"
+	
+)
+
+func main() {
+
+	if config.GetBool("api.production") {
+		gin.SetMode(gin.ReleaseMode)
+	}
+
+	router := gin.Default()
+	corsConfig := cors.DefaultConfig()
+	corsConfig.AllowOrigins = []string{"*"}
+	router.Use(cors.New(corsConfig))
+
+
+	err := database.SetupDatabase()
+	if err != nil {
+		panic(fmt.Errorf("Failed to setup database: %v", err))
+	}
+
+	api := router.Group("/api")
+	{
+		api.GET("/mempool", getMempool)
+		api.GET("/recentBlocks", getRecentBlocks)
+	}
+
+	portString := ":" + config.GetString("api.port")
+	router.Run(portString)
+}
+
+
+func getMempool(c *gin.Context) {
+
+	timestamp, byCount, megabyteMarkersJSON, mempoolSize, err := database.GetMempool()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	// Possible REFACTOR: write to database as blob not JSON String to 
+	// skip the marshalling when writing and unmarshalling when reading
+	// from the database
+ 	var feerateMap map[int]int
+	json.Unmarshal([]byte(byCount), &feerateMap)
+
+	var megabyteMarkers []int
+	json.Unmarshal([]byte(megabyteMarkersJSON), &megabyteMarkers)
+
+	c.JSON(http.StatusOK, gin.H{
+		"timestamp":					timestamp.Unix(),
+		"feerateMap": 				feerateMap,
+		"megabyteMarkers":		megabyteMarkers,
+		"mempoolSize":	 			mempoolSize,
+	})
+}
+
+
+
+func getRecentBlocks(c *gin.Context) {
+
+	blocks, err := database.GetRecentBlocks()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, blocks)
+}
+
---
+++ api/go.mod
@@ -0,0 +1,13 @@
+module github.com/0xb10c/memo/api
+
+go 1.12
+
+require (
+	github.com/gin-contrib/cors v1.3.0
+	github.com/gin-gonic/gin v1.4.0
+	github.com/go-sql-driver/mysql v1.4.1
+	github.com/golang/protobuf v1.3.1
+	github.com/spf13/viper v1.3.2
+)
+
+replace github.com/ugorji/go v1.1.4 => github.com/ugorji/go/codec v0.0.0-20190204201341-e444a5086c43
